4dc99750c74609e60709838f716744c28537d890
Mateus Lima <124712760+mateuslima0@users.noreply.github.com>
2026-08-02 13:54:38 -0300

    Update 1099_sequencia_de_numeros_e_soma.py

--- iniciante/1099_sequencia_de_numeros_e_soma.py
+++ iniciante/1099_sequencia_de_numeros_e_soma.py
@@ -1,8 +1,3 @@
-# URI/Beecrowd 1099 - Sum of Consecutive Odd Numbers II
-# https://www.beecrowd.com.br/judge/pt/problems/view/1099
-# A soma dos N primeiros numeros impares e igual a N ao quadrado.
-# O problema tem varios casos de teste ate o fim da entrada (EOF).
-
 import sys
 
 for linha in sys.stdin:
