4385b03babde789681c2e54db98fa5096a195574
Mateus Lima <124712760+mateuslima0@users.noreply.github.com>
2026-08-02 13:51:59 -0300

    Update 1017_gasto_combustivel.py

--- iniciante/1017_gasto_combustivel.py
+++ iniciante/1017_gasto_combustivel.py
@@ -1,7 +1,3 @@
-# URI/Beecrowd 1017 - Fuel Spent
-# https://www.beecrowd.com.br/judge/pt/problems/view/1017
-# Considera consumo fixo de 12 km por litro
-
 distancia = float(input())
 preco_litro = float(input())
 
