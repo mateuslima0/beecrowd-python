1ccc973ce7114d8a21cfea96f7a843c82dcc8ada
Mateus Lima <124712760+mateuslima0@users.noreply.github.com>
2026-08-02 13:54:59 -0300

    Update 1133_resto_de_uma_divisao.py

--- iniciante/1133_resto_de_uma_divisao.py
+++ iniciante/1133_resto_de_uma_divisao.py
@@ -1,7 +1,3 @@
-# URI/Beecrowd 1133 - Rest of a Division
-# https://www.beecrowd.com.br/judge/pt/problems/view/1133
-# Le pares de valores ate encontrar "0 0", que encerra a entrada
-
 while True:
     a, b = map(int, input().split())
     if a == 0 and b == 0:
